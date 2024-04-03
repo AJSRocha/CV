89f38a57d24f4cfbf04d88c879e1b59d91fa58b4
Arocha <alberto.jmas.rocha@gmail.com>
2024-04-03 11:05:12 +0100

    reestrutura

---
+++ data.r
@@ -0,0 +1,11 @@
+library(tibble)
+experiencia = tribble(
+  ~ Degree, ~ Year, ~ Institution, ~ Where, ~ Detail,
+  "Técnico Superior", "2019 Presente", "Instituto Português do Mar e da Atmosfera", "Matosinhos, Portugal", 
+  list("Projectos: PNAB/DCF, PPCENTRO", "Análise de dados dos stocks de lulas (*Loligo vulgaris*) e potas (*Ommastrephidaes spp.*)"),
+ 
+  "Bolseiro Técnico de Investigação - Nível 1", "2014-2019", "Instituto Português do Mar e da Atmosfera", "Matosinhos, Portugal","",
+  "Bolseiro de Apoio Técnico - Nível 2", "2009-2014", "Instituto Português do Mar e da Atmosfera", "Matosinhos, Portugal","",
+  "Estagiário (MSc)", "2008", "Instituto Português do Mar e da Atmosfera", "Matosinhos, Portugal","",
+  "Estagiário (Lic)", "2007", "Instituto Português do Mar e da Atmosfera", "Matosinhos, Portugal",""
+)
